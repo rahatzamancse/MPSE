Returnable figure plots, fixed weight calculation bugs

MPSE/mview/setup.py
@@ -81,8 +81,8 @@ def setup_weights(distances, weights, max_weight=2.0, min_weight=1e-4):
             weights = np.array([weights(dist) for dist in distances])
     elif isinstance(weights,np.ndarray):
         if len(weights) == distance.num_obs_y(distances):
-            assert min(weights) >= 0
-            weights = np.array([weights])
+            assert np.min(weights) >= 0
+            weights = np.array(weights)
             weights = weights.T * weights
             weights = distance.squareform(weights, checks=False)
         assert distances.shape == weights.shape