Major improvement on fixed and variable projection calculation

MPSE/mview/mpse.py
@@ -193,13 +193,13 @@ class MPSE(object):
             if self.verbose > 0:
                 print('  setup visualization instance for perspective',
                       self.perspective_labels[i],':')
-            if visualization_method[i] is 'mds':
+            if visualization_method[i] == 'mds':
                 vis = mds.MDS(self.distances[i],
                               weights = self.weights[i],
                               embedding_dimension=self.image_dimension,
                               verbose=self.verbose, indent=self.indent+'    ',
                               **visualization_args[i])
-            elif visualization_method[i] is 'tsne':
+            elif visualization_method[i] == 'tsne':
                 vis = tsne.TSNE(self.distances[i],
                                 embedding_dimension=self.image_dimension,
                                 verbose=self.verbose, indent=self.indent+'    ',
@@ -266,7 +266,7 @@ class MPSE(object):
                         Y[k],batch_size=batch_size,indices=indices)
                     individual_costs[k] = cost_k
                     if return_embedding:
-                        dX += dY_k @ projections[k]
+                        dX += dY_k @ projections[k][:2, :3]
                     if return_projections:
                         dQ.append(dY_k.T @ embedding)
                 if return_embedding:
@@ -327,6 +327,8 @@ class MPSE(object):
             if isinstance(fixed_projections,str):
                 fixed_projections = self.proj.generate(number= \
                             self.n_perspectives,method=fixed_projections)
+            assert(all([isinstance(fp,np.ndarray) for fp in fixed_projections]))
+            fixed_projections = [f[:2, :3] for f in fixed_projections]
             self.projections = fixed_projections
             self.initial_projections = fixed_projections
             self.fixed_projections = True
@@ -349,6 +351,8 @@ class MPSE(object):
                 self.initial_projections = initial_projections
             self.projections = self.initial_projections
             self.fixed_projections = False        
+        print(indent+' Projection is:')
+        print(self.projections)
 
         self.initial_cost = None
         self.initial_individual_cost = None

MPSE/mview/multigraph.py
@@ -980,10 +980,10 @@ def dmatrices(X,input_type='coordinates',**kwargs):
     assert input_type in ['coordinates','similarities']
     K = len(X); N = len(X[0])
     D = np.empty((K,N,N))
-    if input_type is 'coordinates':
+    if input_type == 'coordinates':
         for k in range(K):
             D[k] = coord2dist(X[k],**kwargs)
-    elif input_type is 'similarities':
+    elif input_type == 'similarities':
         for k in range(K):
             D[k] = sim2dist(X[k],**kwargs)
     return D

MPSE/mview/projections.py
@@ -85,7 +85,7 @@ class PROJ(object):
         if self.constraint is None:
             self.c = lambda x : x
             self.random = lambda : np.random.randn(self.d2,self.d1)
-        elif self.constraint is 'orthogonal':
+        elif self.constraint == 'orthogonal':
             def c(P):
                 """\
                 Returns nearest orthogonal matrix to P, that is, Q minimizing 
@@ -96,7 +96,7 @@ class PROJ(object):
             self.c = c
             self.random = lambda : scipy.stats.ortho_group.rvs(self.d1) \
                           [0:self.d2,:]
-        elif self.constraint is 'similar':
+        elif self.constraint == 'similar':
             def c(P):
                 """\
                 Returns nearest scaled orthogonal matrix to P, that is, Q 
@@ -162,6 +162,8 @@ class PROJ(object):
         Y : array or list
         Projected coordinates p_q(X) or [p_q1(X),...,p_qK(X)].
         """
+
+
         if isinstance(q,np.ndarray) and q.shape==self.shape:
             Y = self.P(q,X)
         else:
@@ -207,7 +209,7 @@ class PROJ(object):
         projection/perspective families are ['identity','random']. Other choices
         are dependent on the family and constraints.
         """
-        if method is None:
+        if not method:
             method = 'random'
         if method == 'random':
             Q = []